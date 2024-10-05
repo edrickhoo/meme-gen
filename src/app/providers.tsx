3e88a1866d26cbd74833da5769304396c5dd716d
edric.khoo <edric.khoo@hotmail.com>
2024-10-05 15:07:50 +1000

    fix upload to use origin url

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,7 +7,7 @@ const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 const authenticator = async () => {
   try {
-    const response = await fetch("http://localhost:3000/api/auth");
+    const response = await fetch(`${window.location.origin}/api/auth`);
 
     if (!response.ok) {
       const errorText = await response.text();
